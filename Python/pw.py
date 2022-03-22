7ad437576adca33ae45e08eb362f2d9bcacc6477
CreativeBlaster <icaro-sol@hotmail.com>
2022-03-22 19:04:11 -0300

    commit do final do trabalho. agora é ir pra casa

---
+++ Python/pw.py
@@ -0,0 +1,6 @@
+#! python3
+# pw.py - um programa para repositorio de senhas que nao é seguro
+
+PASSWORDS = {'email' : 'F7minlBDDuvMJuxESSKHFhTxFtjVB6',
+            'blog' : 'Vmal...',
+            'luggage' : '12345'}
