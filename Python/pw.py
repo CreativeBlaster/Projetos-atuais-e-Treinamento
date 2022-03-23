13651aee96519ba0ea2fcf64c7fba1aecae648af
CreativeBlaster <icaro-sol@hotmail.com>
2022-03-23 16:48:42 -0300

    aula de HTML5 e CSS concluida com sucesso

--- Python/pw.py
+++ Python/pw.py
@@ -4,3 +4,8 @@
 PASSWORDS = {'email' : 'F7minlBDDuvMJuxESSKHFhTxFtjVB6',
             'blog' : 'Vmal...',
             'luggage' : '12345'} 
+import sys
+if len(sys.argv) < 2:
+    print('usage: python pw.py [account] - copy account password')
+    sys.exit()
+account = sys.argv[1]   #o primeiro argumento da linha de comando é o nome da conta
